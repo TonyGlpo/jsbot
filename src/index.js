3a090b1714c4ff3b3c4f9862d5b07b356dd66b17
Antônio Gabriel <antoniogabrielleal@gmail.com>
2019-07-28 12:39:39 -0300

    add cmds and event loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 const Discord = require('discord.js'),
-const client = new Discord.Client();
+client = new Discord.Client();
+const Enmap = require('enmap'), fs = require('fs-extra');
+client.commands = new Enmap();
 require('dotenv').config();
 
+const init = async () => {
+        const commandFiles = await fs.readdir('./commands');
+        console.log(`Carregando o total de ${commandFiles.length} comandos.`);
+        commandFiles.shift();
+        commandFiles.forEach(file => {
+            try {
+                const props = require(`./commands/${file}`);
+                if(file.split('.').slice(1)[0] !== 'js') return;
+                if (props.init) {
+                    props.init(client);
+                }
+                client.commands.set(props.command.name, props);
+            } catch (error) {
+                console.log(`[#ERROR] Impossivel executar comando ${file}: ${error}`);
+            }
+        })
+        const eventFiles = await fs.readdir('./events');
+        console.log(`Carregando o total de ${eventFiles.length} eventos.`);
+        eventFiles.forEach(file => {
+            const eventName = file.split('.')[0];
+            const event = require(`./events/${file}`);
+            client.on(eventName, event.bind(null, client));
+        })
+}
+
+module.exports = client.commands;
 
 client.login(process.env.TOKEN)
 console.log('Bot is running!')
+init();
